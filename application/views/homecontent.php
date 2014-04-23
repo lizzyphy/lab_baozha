d4f033f63c6a23e778e540c8706a511c33589447
Mld <463471808@qq.com>
2014-04-23 22:51:00 +0800

    适应IE6

--- application/views/homecontent.php
+++ application/views/homecontent.php
@@ -5,7 +5,7 @@
 			<ul>
 				<li>
 				<a href=""><img width="420" height="350" src="<?php echo base_url('static/img/1.jpg'); ?>" /></a>
-				<p class="pic_title" ><a href="" style="color: #666;">爆炸科学与技术国家重点实验室2014工作会议</a></p>
+
 				</li>	
 				<li>
 				<a href=""><img width="420" height="350" src="<?php echo base_url('static/img/2.jpg'); ?>" /></a>
@@ -34,41 +34,43 @@
 			<span style="font-family: Microsoft YaHei; font-size: 16px;">新闻快讯</span>
 		</div>
 		<ul>
-			<li><span><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室2014工作会议">爆炸科学与技术国家重点实验室2014工作会议</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室2014工作会议">爆炸科学与技术国家重点实验室2014工作会议</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="科技后备人才早期培养计划师生见面会">科技后备人才早期培养计划师生见面会</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="科技后备人才早期培养计划师生见面会">科技后备人才早期培养计划师生见面会</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议">爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议">爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="“爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开">爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="“爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开">爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
+					<div class="clear"></div>
 		</ul>
 		<div class="title">
 			<span style="font-family: Microsoft YaHei; font-size: 16px;">最新通知</span>
 		</div>
 		<ul>
-			<li><span><a href="www.baidu.com" title="第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为创新研究群体学术研讨会第一轮通知">第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为...</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为创新研究群体学术研讨会第一轮通知">第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为...</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="2014年度开放基金项目资助通知－新">2014年度开放基金项目资助通知－新</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="2014年度开放基金项目资助通知－新">2014年度开放基金项目资助通知－新</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="第五届学术委员会第3次会议">第五届学术委员会第3次会议</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="第五届学术委员会第3次会议">第五届学术委员会第3次会议</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
+			<li><span class="right_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
+			<li><span class="right_span"><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
 			2013 International Workshop on Intensive Loading and Its Effects &...</a></span>
 			<span class="time">2014-3-10</span>
+			<div class="clear"></div>
 			</li>
 		</ul>
 		<div class="clear"></div>
@@ -79,22 +81,23 @@
 			<span style="font-family: Microsoft YaHei; font-size: 16px;">专家讲坛信息</span>
 		</div>
 		<ul>
-			<li><span><a href="www.baidu.com" title="国际著名爆轰专家到我校国家重点实验室进行学术交流 ">国际著名爆轰专家到我校国家重点实验室进行学术交流 </a></span>
+			<li><span  class="left_span"><a href="www.baidu.com" title="国际著名爆轰专家到我校国家重点实验室进行学术交流 ">国际著名爆轰专家到我校国家重点实验室进行学术交流 </a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色列国际著名学者交流访问">爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色...</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色列国际著名学者交流访问">爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色...</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="国家重点实验室邀请郭林教授为师生做学术报告">国家重点实验室邀请郭林教授为师生做学术报告</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="国家重点实验室邀请郭林教授为师生做学术报告">国家重点实验室邀请郭林教授为师生做学术报告</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
+			<li><span class="left_span"><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
 			2013 International Workshop on Intensive Loading and Its...</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
+			<div class="clear"></div>
 		</ul>
 		<div class="clear"></div>
 	</div>
@@ -104,22 +107,23 @@
 			<span class="right1" style="font-family: Microsoft YaHei; font-size: 16px;">学术会议信息</span>
 		</div>
 		<ul>
-			<li><span><a href="www.baidu.com" title="国际著名爆轰专家到我校国家重点实验室进行学术交流 ">国际著名爆轰专家到我校国家重点实验室进行学术交流 </a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="国际著名爆轰专家到我校国家重点实验室进行学术交流 ">国际著名爆轰专家到我校国家重点实验室进行学术交流 </a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色列国际著名学者交流访问">爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色...</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色列国际著名学者交流访问">爆炸科学与技术国家重点实验室邀请英国、俄罗斯、波兰、以色...</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="国家重点实验室邀请郭林教授为师生做学术报告">国家重点实验室邀请郭林教授为师生做学术报告</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="国家重点实验室邀请郭林教授为师生做学术报告">国家重点实验室邀请郭林教授为师生做学术报告</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
+			<li><span class="left_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
-			<li><span><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
+			<li><span class="left_span"><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
 			2013 International Workshop on Intensive Loading and Its...</a></span>
 			<span class="time">2014-3-10</span>
 			</li>
+			<div class="clear"></div>
 		</ul>
 	</div>
 	<div class="clear"></div>
