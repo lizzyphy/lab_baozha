64f54742590965affb4de26bfc73ac12629406fb
lizzy <lizzy.999@qq.com>
2014-04-24 19:57:09 +0800

    最终版

--- application/views/homeheader.php
+++ application/views/homeheader.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml"/>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>爆炸实验室主页</title>
-<link rel="stylesheet" href="<?php echo base_url('static/css/home.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('static/css/home.css'); ?>"/>
 <link rel=stylesheet type="text/css" href="<?php echo base_url('static/css/css.css'); ?>" />
 <script type="text/javascript" src="<?php echo base_url('static/js/pic.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('static/js/jquery-1.8.0.js'); ?>"></script>
