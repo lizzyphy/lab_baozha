a4a91a519ca8aabd911e1c2c62e6b9194ec3fb07
WT <wangte168@sina.com>
2014-04-27 17:32:16 +0800

    调整tittle部分内容

--- application/views/homecontent.php
+++ application/views/homecontent.php
@@ -29,69 +29,66 @@
 				</ul>
 			</div>
 		</div>
-	<div style="width: 420px; float:left;">
-		<form action="" method="post" >
-              <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                   <tr valign="middle">
-                       <td>
-                        <span class="f_left"><input class="input_ser" type="text" name="username" value="&nbsp;请输入关键字"  
-                        onmouseover=this.focus();this.select();  
-                        onclick="if(value==defaultValue){value='';this.style.color='#000'}"   
-                        onBlur="if(!value){value=defaultValue;this.style.color='#999'}"style="color:#999" /></span>
-                        <input class="input_sub" type="submit" name="submit" id="submit" value="搜索" />  
-                        <div class="clear"></div>
-                        </td>
-                      </tr>
-                    </table>     
-              </form> 
+	<div style="width: 440px; float:left;">
 		<div>
-			<embed src="<?php echo base_url('static/img/爱如潮水.swf'); ?>" width="420" height="350"></embed>
+			<embed src="<?php echo base_url('static/img/爱如潮水.swf'); ?>" width="440" height="350"></embed>
 			<div class="clear"></div>
 		</div>
+		<form action="" method="post" >
+          	<table width="100%" border="0" cellpadding="0" cellspacing="0">
+               <tr valign="middle">
+                   <td>
+                    <span class="f_left"><input class="input_ser" type="text" name="username" value="请输入关键字" onmouseover=this.focus();
+                    onclick="value='';this.style.color='#000'"   
+                    onBlur="if(!value){value=defaultValue;this.style.color='#999'}"style="color:#999" /></span>
+                    <input class="input_sub" type="submit" name="submit" id="submit" value="搜索" />  
+                    <div class="clear"></div>
+                   </td>
+               </tr>
+            </table>     
+        </form> 
 	</div>
 	
 	<div class="right">
 		<div class="title">
-			<span style="font-family: Microsoft YaHei; font-size: 16px;">新闻快讯</span>
+			<span>新闻快讯</span>
 		</div>
 		<ul>
 			<li><span class="right_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室2014工作会议">爆炸科学与技术国家重点实验室2014工作会议</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="科技后备人才早期培养计划师生见面会">科技后备人才早期培养计划师生见面会</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议">爆炸科学与技术国家重点实验室召开第五届学术委员会第2次会议</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="“爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开">爆炸科学与技术国家重点实验室山东研究基地”第一届理事会第三次会议召开</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
-					<div class="clear"></div>
 		</ul>
 		<div class="title">
-			<span style="font-family: Microsoft YaHei; font-size: 16px;">最新通知</span>
+			<span>最新通知</span>
 		</div>
 		<ul>
 			<li><span class="right_span"><a href="www.baidu.com" title="第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为创新研究群体学术研讨会第一轮通知">第六届全国强动载效应及防护学术会议暨2014复杂介质/结构的动态力学行为...</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="2014年度开放基金项目资助通知－新">2014年度开放基金项目资助通知－新</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="第五届学术委员会第3次会议">第五届学术委员会第3次会议</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="青少年科技后备人才早期培养计划（第13期）师生见面会">青少年科技后备人才早期培养计划（第13期）师生见面会</a></span>
-			<span class="time">2014-3-10</span>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 			<li><span class="right_span"><a href="www.baidu.com" title="2013 International Workshop on Intensive Loading and Its Effects & Innovation Research Group Symposium on Dynamic Mechanical Behavior of Complex Media/ Structure">
 			2013 International Workshop on Intensive Loading and Its Effects &...</a></span>
-			<span class="time">2014-3-10</span>
-			<div class="clear"></div>
+			<span class="time">2014-3-10</span><div class="clear"></div>
 			</li>
 		</ul>
 		<div class="clear"></div>
