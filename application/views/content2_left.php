8a1e2480b34604952ffdfc507befad952c84e868
lizzy <lizzy.999@qq.com>
2014-04-29 19:02:47 +0800

    修改二级页面

--- application/views/content2_left.php
+++ application/views/content2_left.php
@@ -1,16 +1,13 @@
 <div class="middle" id="content2_left">
 	<div class="list_left">
-    	<div id="left_head">
-        	<p>实验室概况</p>
-        </div>
         <div id="left_content">
             <ul>
-                <li><a href="index.php">实验室简介</a></li>
-                <li><a href="index.php">发展历程</a></li>
-                <li><a href="index.php">实验室概况</a></li>
-                <li><a href="index.php">现任领导</a></li>
-                <li><a href="index.php">实验室大事记</a></li>
-                <li><a href="index.php">图片展示</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;实验室简介</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;发展历程</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;实验室概况</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;现任领导</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;实验室大事记</a></li>
+                <li><a href="index.php">&nbsp;&nbsp;&nbsp;&nbsp;爆炸安全理论与评估技术部</a></li>
             </ul>
          </div>
     </div>
