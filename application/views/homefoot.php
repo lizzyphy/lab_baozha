55c48b6e7c06c0699d4ae3871f5ca9ec5b4a4c54
lizzy <lizzy.999@qq.com>
2014-05-04 21:30:51 +0800

    修改后台文章类别

--- application/views/homefoot.php
+++ application/views/homefoot.php
@@ -4,6 +4,7 @@
         </div>
         <div class="middle foot1">
             <p>地址：北京市海淀区中关村南大街5号院&nbsp;&nbsp;&nbsp;&nbsp;邮编：100081&nbsp;&nbsp;&nbsp;&nbsp;联系电话：010-6891xxxx&nbsp;&nbsp;&nbsp;&nbsp;传真：010-6891xxxx</p>
+            <p><a href="<?php echo base_url('index.php?d=admin&c=index');?>" target="_blank">管理员登陆</a></p>
         </div>
 </div>
             
