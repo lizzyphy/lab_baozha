db8e8d92ffcbceba030ab0629c8b5b4fb93f2cc3
Mld <463471808@qq.com>
2014-05-10 17:58:21 +0800

    article_2

---
+++ application/views/article2.php
@@ -0,0 +1,13 @@
+   <div class="list_right">
+    	<div id="right_head">
+        	<p><a href="<?php echo base_url('index'); ?>">首页</a>&nbsp;>&nbsp;<?php echo $name['first_name'];?>&nbsp;>&nbsp;<?php echo $name['second_name'];?></p>
+        	 <img width="700" height="5" src="<?php echo base_url('static/img/title_23.jpg'); ?>" />
+        </div>
+        <div class="right_title">
+        	<p><?php echo $article['title'];?></p>
+        </div>
+        <div class="clear"></div>
+        <div class="right_content"><?php echo $article['content'];?></div>
+    </div>
+    <div class="clear"></div>
+</div>
