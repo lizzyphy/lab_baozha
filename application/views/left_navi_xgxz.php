fce2fb4cc621141d21d1a3712b3865d44453a90d
WT <wangte168@sina.com>
2014-08-06 19:02:30 +0800

    增加相关下载

--- application/views/left_navi_xgxz.php
+++ application/views/left_navi_xgxz.php
@@ -6,6 +6,7 @@
                 <li><a href="<?php echo base_url('article/type/?type=50')?>">&nbsp;&nbsp;&nbsp;&nbsp;自主课题表格</a></li>
                 <li><a href="<?php echo base_url('article/type/?type=72')?>">&nbsp;&nbsp;&nbsp;&nbsp;开放课题表格</a></li>
                 <li><a href="<?php echo base_url('article/type/?type=51')?>">&nbsp;&nbsp;&nbsp;&nbsp;学科建设表格</a></li>
+                <li><a href="<?php echo base_url('article/type/?type=106')?>">&nbsp;&nbsp;&nbsp;&nbsp;相关下载</a></li>
             </ul>
          </div>
     </div>
