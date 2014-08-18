892302e10122ffa255f63351ef9faa868732390a
lizzy <lizzy.999@qq.com>
2014-08-18 22:22:03 +0800

    英文页面top和导航

--- application/views/en_home.php
+++ application/views/en_home.php
@@ -9,8 +9,64 @@
 
 <body>
     <div class="whole middle">
+    	<div class="en_top">
+        	<div id="top_l">
+                <a href="index/english"><img src="<?php echo base_url('static/img/top_a01.jpg'); ?>" /></a>
+            </div>
+            <div id="top_r">
+                <span class="top_rr"><a href="<?php echo base_url('index.php?d=admin&c=index');?>"> OA System</a> &nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo base_url('/'); ?>">中文版</a></span>
+            </div>
+            <div class="clear"></div>
+        </div>
         <div class="left_navi">
-            nihao!
+            <div class="en_navi">
+            	<a href="">Home</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Introduction</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">History</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Main Research Direction and Contents</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Organization Structure</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Academic Committee</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Main Research Achievements</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Instruments</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Education</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Main Members of Staff</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Opening Fund</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Opening and Communication</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Contact Numbers of Main Members</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">News&Trends</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Vedio</a>
+            </div>
+            <div class="en_navi">
+            	<a href="">Photos</a>
+            </div>
         </div>
         <div class="right_content">
             内容
