43d75e3662936b477de867dfa7347e00e124c45e
lizzy <lizzy.999@qq.com>
2014-08-22 15:16:05 +0800

    修改英文主页footer

--- application/views/en_footer.php
+++ application/views/en_footer.php
@@ -2,6 +2,10 @@
      	<div class="middle foot1">
         	<p>Copyright © 2009-2014&nbsp;&nbsp;&nbsp;&nbsp;State Key Laboratory of Explosion Science and Technology &nbsp;&nbsp;&nbsp;&nbsp;All Rights Reserved</p>
         </div>
+        <div class="middle foot1">
+            <p>Address：no.5 of zhongguancun south street Haidian district Beijing&nbsp;&nbsp;&nbsp;&nbsp;Postcode：100081&nbsp;&nbsp;&nbsp;&nbsp;Telephone：010－68913957&nbsp;&nbsp;&nbsp;&nbsp;Fax：010－68914267</p>
+
+        </div>
   </div>
 </div>        
 </body>
